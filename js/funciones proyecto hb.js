808df647fb9147afb8aa88a442a6db8c6e963d59
Agustín Cabero <agustincabero@gmail.com>
2018-05-02 22:52:04 -0300

    Declaración de las funciones Extracción, depósito y cambio de limite de extracción.

---
+++ js/funciones proyecto hb.js
@@ -0,0 +1,11 @@
+var saldoCuenta = 30000;
+var limiteExtraccion = 5000;
+
+function increaseBalance(monto){
+  saldoCuenta += monto;
+}
+
+function decreaseBalance(monto){
+  saldoCuenta -= monto;
+}
+
--- js/script.js
+++ js/script.js
@@ -10,15 +10,27 @@ actualizarLimiteEnPantalla();
 
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-
+    var previousBalance = saldoCuenta;
+    var nuevoLimite = parseInt(prompt('Ingrese su nuevo limite de extracción:'));
+    limiteExtraccion = nuevoLimite;
+    actualizarLimiteEnPantalla();
+    alert('Su nuevo limite de extracción es de: $' + limiteExtraccion);
 }
 
 function extraerDinero() {
-
+    var previousBalance = saldoCuenta;
+    var monto = parseInt(prompt('Ingrese el monto a extraer:'));
+    decreaseBalance(monto);
+    actualizarSaldoEnPantalla();
+    alert('Has retirado: $' + monto +'\nSaldo anterior: $' + previousBalance + '\nSaldo actual: $' + saldoCuenta);
 }
 
 function depositarDinero() {
-
+    var previousBalance = saldoCuenta;
+    var monto = parseInt(prompt('Ingrese el monto a depositar:'));
+    increaseBalance(monto);
+    actualizarSaldoEnPantalla();
+    alert('Has depositado: $' + monto +'\nSaldo anterior: $' + previousBalance + '\nSaldo actual: $' + saldoCuenta);
 }
 
 function pagarServicio() {
@@ -35,14 +47,22 @@ function iniciarSesion() {
 
 //Funciones que actualizan el valor de las variables en el HTML
 function cargarNombreEnPantalla() {
-    document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
+document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
 }
 
 function actualizarSaldoEnPantalla() {
-    document.getElementById("saldo-cuenta").innerHTML = "$" + saldoCuenta;
+document.getElementById("saldo-cuenta").innerHTML = "$" + saldoCuenta;
 }
 
 function actualizarLimiteEnPantalla() {
-    document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
+document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
 }
 
+//Funciones creadas por alumno
+function increaseBalance(monto){
+    saldoCuenta += monto;
+}
+
+function decreaseBalance(monto){
+    saldoCuenta -= monto;
+}
