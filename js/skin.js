086da7392373031006727ed941ab323d29a6649c
Agustín Cabero <agustincabero@gmail.com>
2018-05-20 14:08:49 -0300

    Se le agrega un boton para cambiar al modo "noche" en la app.

---
+++ js/skin.js
@@ -0,0 +1,12 @@
+//En este archivo esta todo el JS necesario para modificar el skin del Home Banking
+
+document.getElementById('switch').addEventListener('click', changeMod);
+
+function changeMod() {
+  document.body.classList.toggle('night-mode');
+  document.getElementById('nombre').classList.toggle('night-mode');  
+  var whiteContainer = document.getElementsByClassName('white-container');
+  whiteContainer[0].classList.toggle('night-mode');
+  var tuCuenta = document.getElementsByClassName('tu-cuenta');
+  tuCuenta[0].classList.toggle('night-mode');
+}
